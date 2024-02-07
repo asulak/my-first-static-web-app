981387fd499a2b94ec4f142b1fc02b9294c2e2cc
corporateservicesmonarch <114939929+corporateservicesmonarch@users.noreply.github.com>
2024-02-07 16:55:30 -0700

    Relative file paths
    Relative file path

--- src/script.js
+++ src/script.js
@@ -18,8 +18,8 @@ let typedMessage = null; // Variable to store user's message. The default value 
 // // AZURE SECRET
 
 // // Include required dependencies
-import DefaultAzureCredential from './azure-identity';
-import SecretClient from './azure-keyvault-secrets';
+import {DefaultAzureCredential} from './node_modules/@azure/identity';
+import SecretClient from './node_modules/@azure/keyvault-secrets';
 
 //import {DefaultAzureCredential} from "./node_modules/@azure/identity";
 //import {SecretClient} from "./node_modules/@azure/keyvault-secrets";
